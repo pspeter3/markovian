a0aab7d65ef7234498312edcf5fa44cfc2e17ade
Phips Peter <pspeter333@gmail.com>
2015-04-11 09:19:12 -0700

    Adding a Next method
    This creates a new Ngram

--- ngram.go
+++ ngram.go
@@ -10,6 +10,14 @@ func (n Ngram) ToKey() string {
 	return strings.Join(n, " ")
 }
 
+func (n Ngram) Next(nextWord string) Ngram {
+	size := len(n)
+	ngram := make(Ngram, size)
+	copy(ngram, n[1:])
+	ngram[size-1] = nextWord
+	return ngram
+}
+
 func NewNgram(text string) Ngram {
 	return strings.Split(text, " ")
 }

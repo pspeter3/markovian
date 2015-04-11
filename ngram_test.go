036fac3391d96af460757b3c487a447a0dc25700
Phips Peter <pspeter333@gmail.com>
2015-04-11 09:05:14 -0700

    Adding a simple ngram
    I'm not sure if I want to add a Shift method or make this immutable.

---
+++ ngram_test.go
@@ -0,0 +1,25 @@
+package markovian_test
+
+import (
+	"github.com/pspeter3/markovian"
+	"testing"
+)
+
+func TestConstructor(t *testing.T) {
+	ngram := markovian.NewNgram("hello world")
+	if ngram[0] != "hello" {
+		t.Fatalf("First value was %s", ngram[0])
+	}
+	if ngram[1] != "world" {
+		t.Fatalf("Second value was %s", ngram[1])
+	}
+}
+
+func TestToKey(t *testing.T) {
+	ngram := make(markovian.Ngram, 2)
+	ngram[0] = "hello"
+	ngram[1] = "world"
+	if ngram.ToKey() != "hello world" {
+		t.Fatalf("%s was not hello world", ngram.ToKey())
+	}
+}
